d7df51a24b02b1dce2f3d8a6ec79877fb539463d
Twilightsnow <3278546510@qq.com>
2023-07-20 23:40:19 +0800

    0.01

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
+// 加载全局样式
+import '@/style/index.less'
 
 Vue.config.productionTip = false
 
